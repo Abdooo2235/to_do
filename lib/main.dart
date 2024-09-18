55794f04f409d8eabcba2a90afc41f1fd27841d1
Abdooo2235 <abdalrhmantheiv15@gmail.com>
2024-09-18 15:20:52 +0200

    TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -9,16 +11,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeScreen());
   }
 }
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -31,54 +33,37 @@ class _MyHomePageState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 400,
-        color: Colors.black.withOpacity(0.2),
-        child: Center(
-          child: Padding(
-              padding: const EdgeInsets.all(0.24),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.redAccent,
-                    width: 2,
+      appBar: AppBar(
+        title: const Text('TODO'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: const DefaultTabController(
+        length: 2,
+        child: Column(
+          children: [
+            TabBar(
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.green,
+                indicatorColor: Colors.blue,
+                tabs: [
+                  Tab(
+                    text: "Waiting",
                   ),
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.15),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: const Offset(10, 10),
-                    )
-                  ],
+                  Tab(
+                    text: "Completed",
+                  )
+                ]),
+            Expanded(
+              child: TabBarView(children: [
+                Center(
+                  child: Text("Wait"),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Container(
-                        width: 80,
-                        height: 80,
-                        color: const Color.fromARGB(255, 85, 46, 9),
-                      ),
-                      Container(
-                        width: 80,
-                        height: 80,
-                        color: const Color.fromARGB(255, 33, 243, 121),
-                      ),
-                      Container(
-                        width: 80,
-                        height: 80,
-                        color: const Color.fromARGB(255, 166, 33, 243),
-                      ),
-                    ],
-                  ),
-                ),
-              )),
+                Center(
+                  child: Text("Comp"),
+                )
+              ]),
+            )
+          ],
         ),
       ),
     );
